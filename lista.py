6eab590311f0c1ae47e7f4a3df40ecf3c4504fee
douglasmsi <douglasmsi@gmail.com>
2018-06-26 20:45:00 -0300

    commit

--- lista.py
+++ lista.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-nomes = ['daniel','maria','jose','joao',1,2]
+nomes = ['daniel','maria','jose','joao']
+
+nomes.append(['daniel','prata'])
+nomes.insert(0, 'prata')
+
+print(nomes)
+print(type(nomes))
+print(nomes[-1])
+print(type(nomes[-1]))
+
+print(nomes[-1][0])
+
+print(len(nomes))
+print(len(nomes[-1]))
 
 
-print(nomes[0])
-print(nomes[3])
-print(nomes[1])
-print(nomes[2])
-print(nomes[4])
-print(nomes[5])
-print(nomes[8])
+
+numeros = list((range(40,100)))
+print(numeros)
---
+++ par.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#Ler numero e verificar se ele e par ou impar
+# e adicionar ele em uma lista com o Resultado
+#[2, 'par']
+#[3,'impar']
+#
+entrada = int(input('Numero: '))
+lista = []
+
+if (entrada%2) == 0:
+    lista.insert(0,[entrada,'Par'])
+else:
+    lista.insert(0,[entrada,'Impar'])
+
+print(lista)
