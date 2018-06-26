14b110c4bc2131cae5149f381b61df7d752bb023
douglasmsi <douglasmsi@gmail.com>
2018-06-26 20:06:33 -0300

    commit

--- media.py
+++ media.py
@@ -5,5 +5,17 @@ n1 = float(input("Nota 1:"))
 n2 = float(input("Nota 2:"))
 media = (n1+n2)/2.0
 
-aluno1 = aluno.strip()
-print ("{} a sua nota media e {} {}".format(aluno,media, aluno1))
+if media >= 7.0:
+    result = 'Aprovado'
+elif media < 7.0 and media > 4.0:
+    result = "Recuperacao"
+else:
+    result = 'Reprovado'
+
+print("""
+        Aluno: {}
+        Media: {}
+        Resultado: {}
+        """.format(aluno,media, result))
+
+print ("{} a sua nota media e {} foi {}".format(aluno,media, result))
