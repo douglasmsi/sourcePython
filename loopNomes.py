39ad34c607ad549483221f07252b5e6b4fc1b628
douglasmsi <douglasmsi@gmail.com>
2018-06-26 21:18:02 -0300

    commit

---
+++ loopNomes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Lista de nome vazia
+# Ler nomes, adicionar na lista até digitar sair
+#mostrar a lista no final
+lista = []
+while True:
+    valor = input("Digite um nome ou Sair : ")
+    if valor == 'Sair':
+        break
+
+    lista.append(valor)
+    print('Lista: {}'.format(lista))
