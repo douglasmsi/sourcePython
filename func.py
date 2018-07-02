feddbe1e0de190fb1f6c69bea6327cb188a76aeb
douglasmsi <douglasmsi@gmail.com>
2018-07-02 19:49:20 -0300

    commit

---
+++ func.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def somar(x,y):
+    return x + y
+
+b = somar(3,2)
+print(b)
+exit()
+
+def boas_vindas(nome, idade):
+    print('Ola sou o {} tenho {} anos '.format(nome, idade))
+
+
+boas_vindas('douglas',29)
