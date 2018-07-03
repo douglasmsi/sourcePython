341f38330069ffbd5037a0907edc5612339ffc7e
douglasmsi <douglasmsi@gmail.com>
2018-07-02 21:40:17 -0300

    commit

--- func.py
+++ func.py
@@ -3,12 +3,21 @@
 def somar(x,y):
     return x + y
 
-b = somar(3,2)
-print(b)
-exit()
+#b = somar(3,2)
+#print(b)
+#exit()
+
+def par_impar(numero):
+    if (numero%2) == 0:
+        print('Par')
+    else:
+        print('Impar')
+
 
 def boas_vindas(nome, idade):
     print('Ola sou o {} tenho {} anos '.format(nome, idade))
 
 
 boas_vindas('douglas',29)
+entrada = int(input('Numero: '))
+par_impar(entrada)
