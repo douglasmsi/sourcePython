a291679cecad9e78c7b3017f979dd761e307cba1
douglasmsi <douglasmsi@gmail.com>
2018-07-02 21:06:43 -0300

    commit

--- func1.py
+++ func1.py
@@ -9,5 +9,20 @@ def abre_arquivo(nome):
     except Exception as e:
         return "Erro {}".format(e)
 
-arquivo = abre_arquivo(nome)
-print(arquivo)
+
+def escr_arq(nome_arq, conteudo):
+    try:
+        with open(nome_arq,'w') as arq:
+            arq.writelines(conteudo)
+            return True
+    except Exception as e:
+        return "Erro: {}".format(e)
+
+def alterar_lista(lista):
+    return lista
+
+
+#arquivo = abre_arquivo(nome)
+#print(arquivo)
+#nomes = ['daniel\n','teste\n']
+escr_arq('nomes.txt',abre_arquivo(nome))
