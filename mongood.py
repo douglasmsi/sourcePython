1f6e3342cd5031a8482de9db3b1b25c5d8f08aac
Douglas Marra <douglasmsi@gmail.com>
2018-07-05 20:56:24 -0300

    Commit

---
+++ mongood.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from pymongo import MongoClient
+
+try:
+    client = MongoClient()
+    db = client['4linux']
+except Exception as e:
+    print("ERRO:{}".format(e))
+
+dados = db.pessoas.find()
+dados = [x for x in dados]
+print(dados)
