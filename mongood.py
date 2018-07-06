6b835db9a4ea1d1be838b52b139e3a2183b4611a
Douglas Marra <douglasmsi@gmail.com>
2018-07-05 21:18:05 -0300

    Commit

--- mongood.py
+++ mongood.py
@@ -7,6 +7,12 @@ try:
 except Exception as e:
     print("ERRO:{}".format(e))
 
+
+db.pessoas.remove()
+db.pessoas.insert({'_id':5, 'nome':'joaozinho'})
+db.pessoas.update({'_id':5}, {'$set':{'sobrenome':'silva'}})
+
+
 dados = db.pessoas.find()
 dados = [x for x in dados]
 print(dados)
